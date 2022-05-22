96401070e677a40de5b952d65e7e2c5a50a63ecd
sathwikgaje <85938598+sathwikgaje@users.noreply.github.com>
2022-05-22 09:41:27 +0530

    Update settings.py

--- carzone/settings.py
+++ carzone/settings.py
@@ -15,9 +15,7 @@ from pathlib import Path
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
